6edcd7b30f8824936acf989063f2d1ec11ceba6b
Tom Beynon <tom@tombeynon.co.uk>
2013-09-03 21:15:57 +0100

    Simplified batch action handling

--- lib/railgun/resources_controller/batch_actions.rb
+++ lib/railgun/resources_controller/batch_actions.rb
@@ -3,26 +3,20 @@ module Railgun
 		module BatchActions
 		
 			def batch_action
-				send(params[:batch_method].to_sym)
-			end
-			
-			def batch_delete
-				batch! do |selection|
-	    		resource_class.find(selection).each do |resource|
-	    			resource.delete
-	    		end
-	    	end
-    	end
-    	
-    	def batch! options={}, &block
-				selection = params[:batch_select].map{|id| id.to_i }
-				yield(selection) if block_given?
+				selection = (params[:batch_select] || []).map{|id| id.to_i }
+				send params[:batch_method].to_sym, selection
 				respond_with(collection) do |format|
 					flash[:notice] = selection.count.to_s+" "+railgun_resource.name.downcase.pluralize+" affected"
 					format.html { redirect_to :action => :index}
 				end
 			end
 			
+			def batch_delete selection
+				resource_class.find(selection).each do |resource|
+    			resource.delete
+    		end
+    	end
+    
 		end				
 	end
 end
