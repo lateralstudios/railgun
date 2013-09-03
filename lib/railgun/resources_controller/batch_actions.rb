0d85e7ea0444ec3c4c8afda903fb7c4f94645d93
Tom Beynon <tom@tombeynon.co.uk>
2013-09-04 00:07:51 +0100

    Get the count before we delete them

--- lib/railgun/resources_controller/batch_actions.rb
+++ lib/railgun/resources_controller/batch_actions.rb
@@ -5,12 +5,12 @@ module Railgun
 			def batch_action
 				ids = (params[:batch_select] || []).map{|id| id.to_i }
 				selection = resource_class.where(:id => ids)
-				
+				number_affected = selection.count
 				# TODO: THIS IS VERY INSECURE. DO NOT DO THIS!
 				send params[:batch_method].to_sym, selection
 				
 				respond_with(selection) do |format|
-					flash[:notice] = selection.count.to_s+" "+railgun_resource.name.downcase.pluralize+" affected"
+					flash[:notice] = number_affected.to_s+" "+railgun_resource.name.downcase.pluralize+" affected"
 					format.html { redirect_to :action => :index}
 				end
 			end
