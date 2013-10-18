cbb69e1780fb4bfde126896b0bbb3dfa6a36a7ca
Tom Beynon <tom@tombeynon.co.uk>
2013-10-18 22:21:34 +0100

    Removed deprecated interface methods

--- app/controllers/railgun/dashboard_controller.rb
+++ app/controllers/railgun/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class Railgun::DashboardController < Railgun::RailgunController
 	
 	def index
-		Railgun.interface.add_crumb(:title => "Dashboard", :path => {:controller => 'dashboard', :action => :index} )
-		Railgun.interface.set_title(site_name)
+		add_crumb(:title => "Dashboard", :path => {:controller => 'dashboard', :action => :index} )
+		set_title(site_name)
 	end
 
 end 
