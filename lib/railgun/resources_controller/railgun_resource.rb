aefb4185546fb3cfe300a0459a345d35b638b2f1
Tom Beynon <tombeynon@gmail.com>
2014-05-08 21:07:30 +0100

    Re-wrote resource methods and improved ordering

--- lib/railgun/resources_controller/railgun_resource.rb
+++ lib/railgun/resources_controller/railgun_resource.rb
@@ -1,36 +1,36 @@
 module Railgun
-		module RailgunResource
+  module RailgunResource
 
-			attr_accessor :railgun_resource
+    attr_accessor :railgun_resource
 
-			def load_railgun_resource
-				self.railgun_resource ||= Railgun.application.resource_from_controller(self)
-				override_resource_class_methods! if railgun_resource
-			end
+    def load_railgun_resource
+      self.railgun_resource ||= Railgun.application.resource_from_controller(self)
+      override_resource_class_methods! if railgun_resource
+    end
 
-			def railgun_resource=(resource)
-				@railgun_resource = resource
-				unless resource.nil?
-					# Set inherited_resources
-					defaults :resource_class => resource.resource_class
-	      	#, :route_prefix => resource.route_prefix, :instance_name => resource.resource_name.singular
-        end
-      end
-
-      def override_resource_class_methods!
-      	self.class_eval do
-	      	# Unset inherited_resource's method
-	      	def self.resource_class=(klass); end
-
-	      	def self.resource_class
-	      		@railgun_resource ? @railgun_resource.resource_class : nil
-	      	end
-	      end
-      end
-
-      def inherited(base)
-        super(base)
-        base.load_railgun_resource
+    def railgun_resource=(resource)
+      @railgun_resource = resource
+      unless resource.nil?
+        # Set inherited_resources
+        defaults :resource_class => resource.resource_class
+        #, :route_prefix => resource.route_prefix, :instance_name => resource.resource_name.singular
       end
     end
+
+    def override_resource_class_methods!
+      class_eval do
+        # Unset inherited_resource's method
+        def self.resource_class=(klass); end
+
+        def self.resource_class
+          @railgun_resource ? @railgun_resource.resource_class : nil
+        end
+      end
+    end
+
+    def inherited(base)
+      super(base)
+      base.load_railgun_resource
+    end
+  end
 end
--- lib/railgun/resources_controller/resource_methods.rb
+++ lib/railgun/resources_controller/resource_methods.rb
@@ -1,17 +1,20 @@
 module Railgun
-	module ResourceMethods
+  module ResourceMethods
+    def self.included(base)
+      base.instance_eval do
+        helper_method :railgun_resource
+      end
+    end
 
-		def self.included(base)
-			base.instance_eval do
-				helper_method :railgun_resource
-			end
-		end
+    protected
 
-		protected
+    def railgun_resource
+      self.class.railgun_resource
+    end
 
-		def collection
-			get_collection_ivar || begin
-        c = railgun_chain
+    def collection
+      get_collection_ivar || begin
+        c = paginated_chain
         if defined?(ActiveRecord::DeprecatedFinders)
           # ActiveRecord::Base#scoped and ActiveRecord::Relation#all
           # are deprecated in Rails 4. If it's a relation just use
@@ -21,21 +24,24 @@ module Railgun
           set_collection_ivar(c.respond_to?(:scoped) ? c.scoped : c.all)
         end
       end
-		end
+    end
 
-		def railgun_chain
-			scope = inherited_chain
-			scope = scope.page(page).per(per_page) # Apply Kaminari pagination
-			scope
-		end
+    def paginated_chain
+      scope = end_of_railgun_chain
+      scope = scope.page(page).per(per_page) # Apply Kaminari pagination
+      scope
+    end
 
-		def inherited_chain
-			end_of_association_chain
-		end
+    def end_of_railgun_chain
+      railgun_chain.order('created_at DESC')
+    end
 
-		def railgun_resource
-			self.class.railgun_resource
-		end
+    def railgun_chain
+      begin_of_railgun_chain
+    end
 
-	end
+    def begin_of_railgun_chain
+      end_of_association_chain
+    end
+  end
 end
